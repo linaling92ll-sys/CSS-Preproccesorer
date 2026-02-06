ba310e63e339d757e3b46b32b231db860671be87
linaling92ll-sys <linaling92.ll@gmail.com>
2026-02-06 08:35:00 +0100

    Lagt till image-optimizer och börjat skriva HTML-kod

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import { resolve } from 'path'
+import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
 
 export default defineConfig({
     css: {
@@ -12,5 +13,12 @@ export default defineConfig({
         about: resolve(__dirname, 'about.html')
       }
     }
-  }
+  },
+  plugins: [
+    ViteImageOptimizer({
+      png: { quality: 80 },
+      jpeg: { quality: 75 },
+      webp: { quality: 65 }
+    })
+  ]
 })
